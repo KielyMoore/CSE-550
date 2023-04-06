3a8e7a2501b8c16e8fd5907f6cb2b95160ee19f3
Brady Gehrman <btgehr01@louisville.edu>
2023-04-05 21:31:35 -0400

    [brady] added in local time functionality into the data and the UI. Also added a DataPlot class into the project that will hold the functionality needed to plot each data property.

--- Plot.py
+++ Plot.py
@@ -1,37 +1,81 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import statistics
 
 
-class Dataset:
-    def __init__(self, df, n):
+class DataPlot:
+    def __init__(self, df, propertyName):
+        self.df = df
         self.times = df['times']
-        self.values = df['values']
-        self.name = n
+        self.values = df[propertyName]
+        self.graphType = "line"
+        self.propertyName = propertyName
 
-    def plot_data(self):
+    def get_plot(self):
         # create plot
         plt.rcParams.update({'font.size': 8})
-        plt.plot(self.times, self.values, color='maroon')
+
+        if self.graphType == 'line':
+            plt.plot(self.times, self.values, color='maroon')
+        elif self.graphType == 'scatter':
+            plt.scatter(self.times, self.values)
+        elif self.graphType == 'bar':
+            plt.bar(self.times, self.values)
+        elif self.graphType == 'histogram':
+            plt.hist(self.values)
+        elif self.graphType == "box":
+            plt.boxplot(self.values)
 
         # label graph
         plt.xlabel("Time")
-        plt.ylabel(self.name)
-        plt.title("name")
+        plt.ylabel(self.propertyName)
+        plt.title(self.propertyName + " Plot")
 
         # format graph
         plt.xticks(rotation=30)
         # plt.tight_layout()
         plt.subplots_adjust(bottom=0.3)
+        # return the plot itself, so that it can be placed its own widget
+        return plt
 
-        plt.show()
+    def changeGraphType(self, new_graph_type):
+        self.graphType = new_graph_type
+
+    def filterData(self, start_time, end_time):
+        print(
+            "set time and data arrays by filtering down the initial Panda's dataframe (df)")
+        filtered_df = self.filter_dataframe_by_time(
+            self.df, start_time, end_time)
+        self.times = filtered_df["times"]
+        self.values = filtered_df["values"]
+
+    def filter_dataframe_by_time(df: pd.DataFrame, start_time, end_time):
+        df["times"] = pd.to_datetime(df["times"], utc=True)
+        df["times"] = pd.to_datetime(df["times"])
+        filtered_df = df.loc[(df["times"] >= start_time)
+                             & (df["times"] <= end_time)]
+        return filtered_df
+
+    def calculateAverage(self):
+        return float(sum(self.values)) / float(len(self.values))
+
+    def calculateStandardDev(self):
+        statistics.stdev(self.values)
+
+    def calculateMedian(self):
+        return statistics.median(self.values)
+
+    def findMaximum(self):
+        return max(self.values)
+
+    def findMinimum(self):
+        return min(self.values)
 
 
-df = pd.DataFrame({
-    'times': ['2020-01-17T23:48:00Z', '2020-01-17T23:49:00Z', '2020-01-17T23:50:00Z', '2020-01-17T23:51:00Z', '2020-01-17T23:52:00Z', '2020-01-17T23:53:00Z', '2020-01-17T23:54:00Z', '2020-01-17T23:55:00Z', '2020-01-17T23:56:00Z', '2020-01-17T23:57:00Z'],
-    'values': [30.155257, 29.9799, 29.713417, 29.416833, 29.2752, 29.4671, 29.84755, 30.069133, 30.031567, 30.0993]
+# df = pd.DataFrame({
+#     'times': ['2020-01-17T23:48:00Z', '2020-01-17T23:49:00Z', '2020-01-17T23:50:00Z', '2020-01-17T23:51:00Z', '2020-01-17T23:52:00Z', '2020-01-17T23:53:00Z', '2020-01-17T23:54:00Z', '2020-01-17T23:55:00Z', '2020-01-17T23:56:00Z', '2020-01-17T23:57:00Z'],
+#     'values': [30.155257, 29.9799, 29.713417, 29.416833, 29.2752, 29.4671, 29.84755, 30.069133, 30.031567, 30.0993]
+# })
 
-})
-
-thingy = Dataset(df, 'NAME')
-thingy.plot_data()
-
+# plotObject = DataPlot(df, 'Temp avg')
+# plotObject.plot_data()
--- dataloader.py
+++ dataloader.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
+from Plot import DataPlot
+
 
 def loadData(filter):
-    filePath = filter["file_label"] +"/"+ datetime.strptime(filter["start_date"], '%m/%d/%Y').strftime('%Y%m%d') +"/"+ filter["user"] + "/summary.csv"
+    filePath = "./Dataset/" + datetime.strptime(
+        filter["start_date"], '%m/%d/%Y').strftime('%Y%m%d') + "/" + filter["user"] + "/summary.csv"
     data = pd.read_csv(filePath)
     # Ideally data is loaded from a sever and stored locally
-    
+
     columns_to_drop = []
     for column in filter["columns"]:
         if not filter["columns"][column]:
@@ -15,13 +18,59 @@ def loadData(filter):
 
     # Drop unneeded columns
     data = data.drop(columns=columns_to_drop, axis=1)
+
+    if filter["localTime"]:
+        # configure panda's dataframe to create a new column called times that is equal to local time
+        data["times"] = data.apply(calc_time_col, axis=1)
+    else:
+        # configure panda's dataframe to use create a new column called times that is equal to UTC time
+        data["times"] = data["Datetime (UTC)"]
+
+    # Drop the time related columns now that the time is stored in the times column
+    data = data.drop(["Datetime (UTC)", "Timezone (minutes)",
+                     "Unix Timestamp (UTC)"], axis=1)
+
+    print(data.columns)
+
+    print(data[["times", "Acc magnitude avg"]])
+    # return the pandas data frame
     return data
+
+
+def calc_time_col(row):
+    return convert_utc_to_local(row["Datetime (UTC)"], row["Timezone (minutes)"])
+
+
+def convert_utc_to_local(utc_string, offset_minutes):
+    # Parse the input datetime string into a datetime object
+    utc_dt = datetime.strptime(utc_string, '%Y-%m-%dT%H:%M:%SZ')
+    # Convert the UTC datetime to a timezone-aware datetime object
+    utc_dt = utc_dt.replace(tzinfo=timezone.utc)
+    # Convert the timezone offset to a timedelta object
+    offset = timedelta(minutes=offset_minutes)
+    # Convert the datetime to the local timezone
+    local_dt = utc_dt.astimezone(timezone(offset))
+    # Return the local datetime as a string
+    return local_dt.strftime('%Y-%m-%d %H:%M:%S')
+
+
+def createDataPlotObjects(data: pd.DataFrame):
+    dataPlots = []
+    for col_name in data.columns:
+        if col_name != "times":
+            copy = data[["times", col_name]].copy()
+            plotObject = DataPlot(copy, col_name)
+            dataPlots.append(plotObject)
+
+    print(dataPlots)
+    return dataPlots
 
 
 # Acc, Eda, Temp are the only columns that make sense to graph
 # not sure how we would want to visualize the other columns
 
 # TODO fix axis ticks. currently every timestamp is being displayed
+
 
 def plot_Acc(data: pd.DataFrame):
     fig, ax = plt.subplots()
@@ -30,9 +79,10 @@ def plot_Acc(data: pd.DataFrame):
     plt.xlabel("Time")
     plt.ylabel("Acc Mag Avg")
 
-    plt.show() # automatically displays graph in seperate widow
-    
-    return fig # May be needed for imbedding in tkinter
+    plt.show()  # automatically displays graph in seperate widow
+
+    return fig  # May be needed for imbedding in tkinter
+
 
 def plot_Eda(data: pd.DataFrame):
     fig, ax = plt.subplots()
@@ -42,6 +92,7 @@ def plot_Eda(data: pd.DataFrame):
     plt.ylabel("Eda Avg")
     plt.show()
     return fig
+
 
 def plot_Temp(data: pd.DataFrame):
     fig, ax = plt.subplots()
