32d31b2f865fac24e0b4e2bb0c41b8b81c729e53
jamesreilly999 <127165692+jamesreilly999@users.noreply.github.com>
2023-04-17 13:42:51 -0400

    added plot_selected_properties() to ui.py
    it will now pull up a new window upon submission with all the selected attributes graphed and aligned vertically

--- ui.py
+++ ui.py
@@ -4,14 +4,15 @@ import tkinter as tk
 from tkinter import ttk
 from datetime import datetime
 from Plot import DataPlot
+from dataloader import loadData, createDataPlotObjects, plot_Acc, plot_Eda, plot_Temp, pd, plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
-from dataloader import loadData, createDataPlotObjects, plot_Acc, plot_Eda, plot_Temp
 
 datasetPath = "./Dataset/"
 
+
 # get dates for the dates dropdown on the dataloader
-
-
 def get_dates_from_folder():
     # get a list of all items in the directory
     items = os.listdir(datasetPath)
@@ -30,138 +31,63 @@ def plotProperties(dataPlots: List[DataPlot]):
         dataPlot.get_plot().show()
 
 
-def validateDates():
-    errorMessage = ""
-    if start_date.get() > end_date.get():
-        errorMessage = "End Date cannot be greater than the Start Date."
-    return errorMessage
-
-
-def set_users_in_date_period(root):
-    # reset selected user
-    user.set("")
-    # generate an error message if needed
-    errorMessage = validateDates()
-    if errorMessage == "":
-        # get users that are in trials within the selected start and end dates
-        users = get_users_within_time_period(start_date.get(), end_date.get())
-        # Update the dropdown menu with the list of users
-        users_dropdown['menu'].delete(0, 'end')
-        for userString in users:
-            users_dropdown['menu'].add_command(
-                label=userString, command=tk._setit(user, userString))
-        user.set(users[0])
-    else:
-        # empty users dropdown
-        users_dropdown['menu'].delete(0, 'end')
-        # show error message popup
-        showErrorPopup(root, errorMessage)
+def set_date_period():
+    # get users that are in trials within the selected start and end dates
+    # users = get_users_within_time_period(start_date.get(), start_date.get())
+    print("set user's dropdown values based off of selected dates")
+    # TODO: remove all elements from the user dropdown and replace them or destroy the dropdown and recreate one
 
 
 def get_users_within_time_period(start_date: str, end_date: str):
-    start = datetime.strptime(
-        start_date, "%m/%d/%Y").strftime('%Y%m%d')
-    end = datetime.strptime(end_date, "%m/%d/%Y").strftime('%Y%m%d')
     users = set()
-    for date_folder in os.listdir("./Dataset"):
-        if start <= date_folder <= end:
-            for user_folder in os.listdir(os.path.join("Dataset", date_folder)):
-                if os.path.isdir(os.path.join("Dataset", date_folder, user_folder)):
-                    users.add(user_folder)
-    return sorted(list(users))
+    items = os.listdir(datasetPath)
+    for item in items:
+        if (os.path.isdir(os.path.join(datasetPath, item))):
+            # TODO: make sure the date is within the start date and end date, if so add all of its users (subdirectories to the users set)
+            subdirectory_path = os.path.join(datasetPath, item)
+            subdirectory_items = os.listdir(subdirectory_path)
+            for subItem in subdirectory_items:
+                if (os.path.isdir(os.path.join(subdirectory_path, subItem))):
+                    users.add(subItem)
+    return list(users)
 
 
-def validateUserWithinDateRange():
-    usersWithinSpecifiedDates = get_users_within_time_period(
-        start_date.get(), end_date.get())
-    if user.get() in usersWithinSpecifiedDates:
-        return True
-    else:
-        return False
-
-
-def validateForm():
-    errorMessage = ""
-    if start_date.get() > end_date.get():
-        errorMessage = "End Date cannot be greater than the Start Date."
-    elif user.get() == "":
-        errorMessage = "Please select a User to load data for."
-    elif not ACC_Mag.get() and not Eda.get() and not temp.get() and not movement_intensity.get() and not step_count.get() and not rest.get() and not on_wrist.get():
-        errorMessage = "Please select atleast one data property to view."
-    elif not validateUserWithinDateRange():
-        errorMessage = "Selected user doesn't have recorded data within the specified date range."
-    return errorMessage
-
-
-def submit(root):
-    errorMessage = validateForm()
-    if errorMessage == "":
-        filter = {
-            "start_date": start_date.get(),
-            "end_date": end_date.get(),
-            "user": user.get(),
-            "localTime": localTime.get(),
-            "columns": {
-                "Acc magnitude avg": ACC_Mag.get(),
-                "Eda avg": Eda.get(),
-                "Temp avg": temp.get(),
-                "Movement intensity": movement_intensity.get(),
-                "Steps count": step_count.get(),
-                "Rest": rest.get(),
-                "On Wrist": on_wrist.get()
-            }
+def submit(root: tk):
+    filter = {
+        "start_date": start_date.get(),
+        "end_date": end_date.get(),
+        "user": user.get(),
+        "localTime": localTime.get(),
+        "columns": {
+            "Acc magnitude avg": ACC_Mag.get(),
+            "Eda avg": Eda.get(),
+            "Temp avg": temp.get(),
+            "Movement intensity": movement_intensity.get(),
+            "Steps count": step_count.get(),
+            "Rest": rest.get(),
+            "On Wrist": on_wrist.get()
         }
-        print(filter)
-        # create panda's dataframe
-        data = loadData(filter)
-        # create a DataPlot class object for each column property within the pandas dataframe
-        global dataPlots
-        dataPlots = createDataPlotObjects(data)
-        # plot each of the data plots
-        plotProperties(dataPlots)
+    }
 
-    else:
-        showErrorPopup(root, errorMessage)
+    print(filter)
 
+    # create panda's dataframe
+    data = loadData(filter)
 
-def showErrorPopup(root, message):
-    # Disable the main window while the error popup is displayed
-    root.attributes('-disabled', True)
-    # Create the error popup
-    error_popup = tk.Toplevel(root)
-    error_popup.title('Error')
-    error_popup.geometry('350x200')
-    # Set the popup to be on top of other windows
-    error_popup.attributes('-topmost', True)
-    # Set the function to call when the user closes the popup
-    error_popup.protocol("WM_DELETE_WINDOW",
-                         lambda: close_popup(root, error_popup))
-    # Create the message label
-    message_label = ttk.Label(error_popup, text=message, font=(
-        'Arial', 14), wraplength=250, justify='center')
-    message_label.pack(pady=20)
-    # Create the exit button
-    exit_button = ttk.Button(error_popup, text='Exit', style='Exit.TButton',
-                             command=lambda: close_popup(root, error_popup))
-    exit_button.pack(pady=10)
-    # Center the error popup on the root window
-    error_popup.update_idletasks()
-    x = root.winfo_x() + (root.winfo_width() - error_popup.winfo_reqwidth()) / 2
-    y = root.winfo_y() + (root.winfo_height() - error_popup.winfo_reqheight()) / 2
-    error_popup.geometry("+%d+%d" % (x, y))
-    # Wait for the error popup to be closed
-    error_popup.wait_window()
+    # create a DataPlot class object for each column property within the pandas dataframe
+    global dataPlots
+    dataPlots = createDataPlotObjects(data)
 
+    # plot each of the data plots
+    plotProperties(dataPlots)
 
-def close_popup(root, popup):
-    # Enable the main window and destroy the popup
-    root.attributes('-disabled', False)
-    popup.destroy()
-
+    # plot all selected properties simultaneously
+    plot_selected_properties(dataPlots, root)
 
 def create_widgets(root: tk):
 
-    dates = get_dates_from_folder()
+    dummy_dates = get_dates_from_folder()
+    # ['01/18/2020', '01/19/2020', '01/20/2020', '01/21/2020']
 
     # Start date dropdowns
     start_label = ttk.Label(root, text="Start Date:")
@@ -171,7 +97,7 @@ def create_widgets(root: tk):
     start_date = tk.StringVar()
     global start_date_dropdown
     start_date_dropdown = ttk.OptionMenu(
-        root, start_date, dates[0], *dates)
+        root, start_date, dummy_dates[0], *dummy_dates)
     start_date_dropdown.grid(row=4, column=0)
 
     # End date dropdowns
@@ -182,14 +108,16 @@ def create_widgets(root: tk):
     end_date = tk.StringVar()
     global end_date_dropdown
     end_date_dropdown = ttk.OptionMenu(
-        root, end_date, dates[0], *dates)
+        root, end_date, dummy_dates[0], *dummy_dates)
     end_date_dropdown.grid(row=4, column=1)
 
     set_dates_button = ttk.Button(
-        root, text="Set Dates", command=lambda: set_users_in_date_period(root))
+        root, text="Set Dates", command=set_date_period)
     set_dates_button.grid(row=5, column=0)
 
     # User dropdown
+    dummy_users = ['310', '311', '312']
+
     user_label = tk.Label(root, text="User:")
     user_label.grid(row=6, column=0)
 
@@ -197,7 +125,7 @@ def create_widgets(root: tk):
     user = tk.StringVar()
     global users_dropdown
     users_dropdown = ttk.OptionMenu(
-        root, user, '')
+        root, user, dummy_users[0], *dummy_users)
     users_dropdown.grid(row=7, column=0)
 
     global localTime
@@ -254,6 +182,34 @@ def create_widgets(root: tk):
     Wrist_Check.grid(row=11, column=2)
 
     # Submit Button
-    submit_button = ttk.Button(
-        root, text="Submit", command=lambda: submit(root))
+    submit_button = ttk.Button(root, text="Submit", command=lambda: submit(root))
     submit_button.grid(row=12, column=0)
+
+def plot_selected_properties(dataPlots, root):
+    # create popup window for plots
+    new_window = tk.Toplevel(root)
+    new_window.geometry('1000x1000')
+
+    colors = ['red', 'green', 'blue', 'maroon', 'purple', 'pink', 'orange']
+
+    i = 0
+    for dataPlot in dataPlots:
+        # create a Matplotlib figure and axis
+        fig = Figure(figsize=(2, 1), dpi=100)  # create a figure with a size of 5x4 inches and a DPI of 100
+        ax = fig.add_subplot(111)  # create a single subplot within the figure
+
+        # plot some data on the subplot
+        line, = ax.plot(dataPlot.times, dataPlot.values, label=dataPlot.propertyName, color=colors[i])
+        ax.legend(handles=[line], loc='upper left')  # add a legend to the plot
+
+        # create a canvas to display the figure in the Tkinter window
+        canvas = FigureCanvasTkAgg(fig, master=new_window)
+
+        # draw the figure on the canvas
+        canvas.draw()
+
+        # add the canvas to the Tkinter window using grid()
+        canvas.get_tk_widget().pack(side='top', fill='both', expand=True)
+        i += 1
+
+
